fa31d5fb73a104370200e5575c283ddc7ec60d23
Anthony Rojas <anthony.rojas095@gmail.com>
2019-01-06 03:23:57 -0800

    build version of react portfolio

---
+++ precache-manifest.d8e10b7142578ada49e3009a1bbd6401.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e460f3ed5a855afe6286",
+    "url": "/static/css/4.59463f06.chunk.css"
+  },
+  {
+    "revision": "467a21e5458af91076ea",
+    "url": "/static/js/0.467a21e5.chunk.js"
+  },
+  {
+    "revision": "d068e6859f0e55a0f837",
+    "url": "/static/js/main.d068e685.chunk.js"
+  },
+  {
+    "revision": "fa91a29fdb1771eb823954ab4ab69dab",
+    "url": "/static/media/code-background.fa91a29f.png"
+  },
+  {
+    "revision": "20ddeac1315f1075c36a",
+    "url": "/static/js/2.20ddeac1.chunk.js"
+  },
+  {
+    "revision": "dbec69ce6d96ab801d16",
+    "url": "/static/js/runtime~main.dbec69ce.js"
+  },
+  {
+    "revision": "01c506f70f60b8557763",
+    "url": "/static/js/3.01c506f7.chunk.js"
+  },
+  {
+    "revision": "0e6b7650bb920db55688",
+    "url": "/static/js/6.0e6b7650.chunk.js"
+  },
+  {
+    "revision": "e460f3ed5a855afe6286",
+    "url": "/static/js/4.e460f3ed.chunk.js"
+  },
+  {
+    "revision": "6f549f77837ba2b9762f",
+    "url": "/static/js/5.6f549f77.chunk.js"
+  },
+  {
+    "revision": "d068e6859f0e55a0f837",
+    "url": "/static/css/main.873ceb56.chunk.css"
+  },
+  {
+    "revision": "6f549f77837ba2b9762f",
+    "url": "/static/css/5.f4473301.chunk.css"
+  },
+  {
+    "revision": "01c506f70f60b8557763",
+    "url": "/static/css/3.15ac4cc0.chunk.css"
+  },
+  {
+    "revision": "20ddeac1315f1075c36a",
+    "url": "/static/css/2.c9c1641e.chunk.css"
+  },
+  {
+    "revision": "63c35e9395254507e31a87b314d16d89",
+    "url": "/index.html"
+  }
+];
